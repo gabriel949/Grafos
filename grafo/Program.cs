92534d27bf8b32d89107ddcc2c4b7c17e19f41bd
gabriel949 <gabriel.costa949@gmail.com>
2017-08-22 10:31:51 -0300

    first commit

---
+++ grafo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafo
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de vértices : ");
+            int vertice = int.Parse(Console.ReadLine());
+            grafos matriz = new grafos(vertice);
+
+            int opcao = 0;
+
+
+            while (opcao != 4)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Criar arestas.");
+                Console.WriteLine("2 - Remover aresta.");
+                Console.WriteLine("3 - Mostrar Grafo.");
+                Console.WriteLine("4 - Sair.");
+                Console.Write("Escolha uma opção : ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+
+                    case 1:
+
+                        Console.Clear();
+                        Console.WriteLine(" Digite as vertices que terão arestas");
+                        Console.WriteLine(" ====================================");
+                        Console.Write("\n\n Vertice 1 : ");
+                        int vertice1 = int.Parse(Console.ReadLine());
+                        Console.Write("\n\n Vertice 2 : ");
+                        int vertice2 = int.Parse(Console.ReadLine());
+
+
+                        matriz.arestas(vertice1, vertice2);
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine(" Digite as vertices que terão arestas removidas");
+                        Console.WriteLine(" ==============================================");
+                        Console.Write("\n\n Vertice 1 : ");
+                        int verticeRemover1 = int.Parse(Console.ReadLine());
+                        Console.Write("\n\n Vertice 2 : ");
+                        int verticeRemover2 = int.Parse(Console.ReadLine());
+
+
+                        matriz.removerAresstas(verticeRemover1, verticeRemover2);
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        matriz.mostrar(vertice);
+                        break;
+
+
+                }
+
+            }
+
+
+
+        }
+    }
+
+
+}
---
+++ grafo/grafos.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafo
+{
+    class grafos
+    {
+        public int[,] matriz;
+
+        public grafos(int tamanho)
+        {
+            matriz = new int[tamanho, tamanho];
+        }
+
+
+        public void arestas(int a, int b)
+        {
+            matriz[a - 1, b - 1] = 1;
+            matriz[b - 1, a - 1] = 1;
+
+        }
+
+        public void removerAresstas(int a,int b)
+        {
+            matriz[a - 1, b - 1] = 0;
+            matriz[b - 1, a - 1] = 0;
+        }
+
+        public void mostrar(int a)
+        {
+            int cont = 0;
+
+            Console.WriteLine();
+            Console.WriteLine();
+            
+
+            for (int i = 1; i < a+1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("  "+i+" ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < a; i++)
+            {
+                cont++;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(cont);
+
+                for (int j = 0; j < a; j++)
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (matriz[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write("  " + matriz[i, j] + " ");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
